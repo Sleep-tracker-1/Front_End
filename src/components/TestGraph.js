920b7baf503ff19dbfe8c579c9f52cf6b7f4ca89
whoIsJosh_ <joshua.c.luscombe@gmail.com>
2020-02-27 21:27:44 -0800

    achieved basic chart functionality

--- src/components/TestGraph.js
+++ src/components/TestGraph.js
@@ -1,51 +1,98 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
+import ChartDataLabels from "chartjs-plugin-datalabels";
 
 function TestGraph() {
+    const moodAndRestObj = { mood: 8, restfulness: 10 };
     const chartProps = {
         data: {
-            labels: ["M", "T", "W", "T", "F", "S", "S"],
+            labels: [
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday",
+            ],
             datasets: [
                 {
-                    data: [7, 12, 6, 5, 8, 9, 9],
+                    data: [6, 5, 9, 12, 8, 5, 10],
                     label: "Week 1",
-                    value: "zillo",
+                    moodAndRest: { moodAndRestObj },
                     borderColor: "#3e95cd",
                     fill: false,
                     lineTension: 0,
-                    radius: 6,
-                    hoverRadius: 12,
-                },
-                {
-                    data: [1, 2, 3, 4, 5, 6, 7],
-                    label: "Week 2",
-                    borderColor: "#8e5ea2",
-                    fill: false,
-                },
-                {
-                    data: [1, 2, 3, 4, 5, 6, 7],
-                    label: "Week 3",
-                    borderColor: "#3cba9f",
-                    fill: false,
-                },
-                {
-                    data: [1, 2, 3, 4, 5, 6, 7],
-                    label: "Week 4",
-                    borderColor: "#e8c3b9",
-                    fill: false,
-                },
-                {
-                    data: [1, 2, 3, 4, 5, 6, 7],
-                    label: "Week 5",
-                    borderColor: "#c45850",
-                    fill: false,
+                    radius: 30,
+                    hoverRadius: 70,
+                    datalabels: {
+                        labels: {
+                            textStrokeColor: "black",
+                            textStrokeWidth: 8,
+                            labelTextColor: "black",
+                        },
+                    },
                 },
             ],
         },
         options: {
+            scales: {
+                yAxes: [
+                    {
+                        scaleLabel: {
+                            display: true,
+                            labelString: "Hours Slept",
+                            fontSize: 30,
+                        },
+                    },
+                ],
+            },
+            plugins: {
+                // Change options for ALL labels of THIS CHART
+                datalabels: {
+                    color: "#36A2EB",
+                },
+            },
             title: {
                 display: true,
-                text: "Hours Slept",
+                text: "Sleep Quality Tracker",
+                fontSize: 40,
+            },
+            tooltips: {
+                titleFontSize: 14,
+                bodyFontSize: 24,
+                bodySpacing: 20,
+                bodyAlign: "center",
+                caretPadding: 40,
+                mode: "index",
+                callbacks: {
+                    // Use the footer callback to display the sum of the items showing in the tooltip
+
+                    label: function(tooltipItem, data) {
+                        const thisDataset =
+                            data.datasets[Number(tooltipItem.datasetIndex)];
+
+                        const WeekLabel =
+                            "Hours slept: " +
+                            thisDataset.data[Number(tooltipItem.index)];
+
+                        return WeekLabel;
+                    },
+                    afterLabel: function(tooltipItem, data) {
+                        const thisDataset =
+                            data.datasets[Number(tooltipItem.datasetIndex)];
+                        console.log(thisDataset);
+                        const rest =
+                            thisDataset.moodAndRest.moodAndRestObj.restfulness;
+                        const mood =
+                            thisDataset.moodAndRest.moodAndRestObj.restfulness;
+
+                        const stringo = `Rest: ${rest} - Mood: ${mood}`;
+
+                        return stringo;
+                    },
+                },
+                footerFontStyle: "normal",
             },
         },
     };
@@ -54,9 +101,7 @@ function TestGraph() {
         <Line
             ref={chartReference}
             data={chartProps.data}
-            width={100}
-            height={50}
-            options={{ maintainAspectRatio: false }}
+            options={chartProps.options}
         />
     );
 }
