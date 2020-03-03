9f80fef6667ae9799a81e64c041cdce4bf238570
Janessa Garrow <janessa.garrow@gmail.com>
2020-03-02 20:21:18 -0700

    Render IconTab, with necessary component level state and functions

--- src/components/UserLandingPage/LandingPage.js
+++ src/components/UserLandingPage/LandingPage.js
@@ -2,65 +2,125 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 import IconTab from "./IconTab";
+import { WiSunrise } from "react-icons/wi";
+import { FiSun, FiMoon } from "react-icons/fi";
 
-const LandingPage = props => {
-    const [wakeUp, setWakeUp] = useState(null);
-    const [bedtime, setBedtime] = useState(null);
-    const [moods, setMoods] = useState({
-        wakeUp: "",
-        midday: "",
-        bedtime: "",
-    });
-    const [tiredness, setTiredness] = useState({
-        wakeUp: "",
-        midday: "",
-        bedtime: "",
-    });
+const Sunrise = styled(WiSunrise)`
+    font-size: 2.5rem;
+    z-index: 5;
+    position: absolute;
+    right: 0;
+    transform: rotate(90deg);
+`;
 
-    const handleWakeUpChange = e => {
-        setWakeUp(e.target.value);
+const initialValues = {
+    mood: 0,
+    tiredness: 0,
+};
+
+const initialValuesPlusTime = {
+    ...initialValues,
+    time: "",
+};
+
+const LandingPage = () => {
+    const [wakeUpSlide, setWakeUpSlide] = useState(0);
+    const [bedtimeSlide, setBedtimeSlide] = useState(0);
+    const [middaySlide, setMiddaySlide] = useState(0);
+
+    const wakeUpTap = e => {
+        let slidePosition = wakeUpSlide;
+
+        if (slidePosition === 0) {
+            slidePosition += 165;
+        } else {
+            slidePosition -= 165;
+        }
+
+        setWakeUpSlide(slidePosition);
     };
 
-    const handleBedtimeChange = e => {
-        setBedtime(e.target.value);
+    const bedtimeTap = e => {
+        let newPos = bedtimeSlide;
+
+        if (newPos === 0) {
+            newPos -= 165;
+        } else {
+            newPos += 165;
+        }
+
+        setBedtimeSlide(newPos);
     };
 
-    const handleMoodChange = e => {
-        setMoods({
-            ...moods,
-            [e.target.name]: e.target.value,
-        });
+    const middayTap = e => {
+        let newPosition = middaySlide;
+
+        if (newPosition === 0) {
+            newPosition -= 135;
+        } else {
+            newPosition += 135;
+        }
+
+        setMiddaySlide(newPosition);
     };
 
-    const handleTirednessChange = e => {
-        setTiredness({
-            ...tiredness,
-            [e.target.name]: e.target.value,
-        });
-    };
+    // const wakeUpDragConstraints = {
+    //     left: 0,
+    //     right: 165,
+    // };
+
+    // const middayDragConstraints = {
+    //     top: 135,
+    //     bottom: 0,
+    // };
+
+    // const bedtimeDragConstraints = {
+    //     left: -165,
+    //     right: 0,
+    // };
+
+    // const horizontalDrag = "x";
+    // const verticalDrag = "y";
 
     return (
-        <div>
+        <>
             <IconTab
-                initialValue={wakeUp}
+                heading="Wake Up"
+                needsTimeInput={true}
+                timeLabel="Wake up time"
                 timeId="wakeUpTime"
-                moodId="wakeUpMood"
-                tirednessId="wakeUpTiredness"
-                handleChange={handleWakeUpChange}
+                initialValues={initialValuesPlusTime}
+                isWakeUp={true}
+                animateX={wakeUpSlide}
+                tapFunc={wakeUpTap}
+                icon={Sunrise}
+                // dragDirection={horizontalDrag}
+                // dragConstraints={wakeUpDragConstraints}
             />
             <IconTab
-                initialValue={null}
-                moodId="middayMood"
-                tirednessId="middayTiredness"
+                heading="Midday"
+                initialValues={initialValues}
+                icon={FiSun}
+                isMidday={true}
+                animateY={middaySlide}
+                tapFunc={middayTap}
+                // dragDirection={verticalDrag}
+                // dragConstratints={middayDragConstraints}
             />
             <IconTab
-                initialValue={bedtime}
+                heading="Bedtime"
+                needsTimeInput={true}
+                timeLabel="Bedtime"
                 timeId="bedtime"
-                moodId="bedtimeMood"
-                tirednessId="bedtimeTiredness"
-                handleChange={handleBedtimeChange}
+                initialValues={initialValuesPlusTime}
+                isBedtime={true}
+                icon={FiMoon}
+                animateX={bedtimeSlide}
+                tapFunc={bedtimeTap}
+                // dragDirection={horizontalDrag}
+                // dragConstraints={bedtimeDragConstraints}
             />
-        </div>
+        </>
     );
 };
 
