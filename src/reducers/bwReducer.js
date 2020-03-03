19285264eb2e7b855ed0d5e97dfe5b6000fdb2e3
Janessa Garrow <janessa.garrow@gmail.com>
2020-03-02 21:53:38 -0700

    Add baseUrl to axiosWithAuth. Setup initial GET request for user data with reducer and action creator

--- src/reducers/bwReducer.js
+++ src/reducers/bwReducer.js
@@ -1,6 +1,12 @@
+import {
+    FETCHING_USER,
+    FETCH_USER_DATA,
+    ERROR_FETCHING_USER_DATA,
+} from "../actions/bwActions";
+
 const initialState = {
     // initialize your state here
-
+    user: {},
     moodEmojis: {
         great: {
             emoji: "😊",
@@ -29,11 +35,32 @@ const initialState = {
             desc: "Sleeping face",
         },
     },
+    isLoading: false,
+    error: "",
 };
 
 export const bwReducer = (state = initialState, action) => {
     switch (action.type) {
         // your cases here
+        case FETCHING_USER:
+            return {
+                ...state,
+                isLoading: true,
+                error: "",
+            };
+        case FETCH_USER_DATA:
+            return {
+                ...state,
+                user: action.payload,
+                isLoading: false,
+                error: "",
+            };
+        case ERROR_FETCHING_USER_DATA:
+            return {
+                ...state,
+                isLoading: false,
+                error: action.payload,
+            };
         default:
             return state;
     }
