35fd82ce0648be5ce50d808e495ea718500b5b65
Eddie Jimenez <52768061+l8nightswithJS@users.noreply.github.com>
2020-03-04 21:02:27 -0800

    Added Accounts Page

---
+++ src/components/AccountPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const AccountPage = () => {
+    
+    return(
+        <>  
+            <h2>Sleep Tracker</h2>
+            <h1>username</h1>
+            <h3></h3>
+        </>
+    )
+}
+
+export default AccountPage;
